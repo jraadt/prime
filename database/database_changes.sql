90f2cd303f3d7695d817219bd945e3f989d0bd14
Jeremy Raadt <jraadt@minutebids.com>
2014-09-14 23:33:58 -0500

    Fixed sql statement.

--- database/database_changes.sql
+++ database/database_changes.sql
@@ -1,4 +1,4 @@
-alter pitches add month varchar(4);
+alter table pitches add month varchar(4);
 update pitches set month = substr(sv_id, 1, 4);
 create index pitcher_index on atbats (pitcher);
 create index month_index on pitches (month);
